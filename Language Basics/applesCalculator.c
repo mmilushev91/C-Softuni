2417adc62afde33b3290a0b9b58e87ae8e570116
mmilushev91 <milushev_1990@abv.bg>
2025-08-19 11:03:09 +0300

    Create applesCalculator.c

---
+++ Language Basics/applesCalculator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int three1 = 150, three2 = 142, three3 = 127, total;
+    total = three1 + three2 + three3;
+    
+    printf("Apples gathered = %d\n", total);
+    
+    return 0;
+}
